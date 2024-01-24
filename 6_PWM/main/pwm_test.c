27d7c3be813d5b713caaabc43f0c8f18bcf718fd
Zain <beingzainsv@gmail.com>
2024-01-24 16:32:42 +0530

    Refactor PWM task to use motor handles and implement ramping

--- 6_PWM/main/pwm_test.c
+++ 6_PWM/main/pwm_test.c
@@ -1,108 +1,45 @@
-//C Headers
+// C Headers
 #include <stdio.h>
 #include <math.h>
 #include <time.h>
 #include <string.h>
 #include <stdlib.h>
 
-//Components
+// Components
 #include "sra_board.h"
 
 // Select the mode needed by uncommenting its definition
 #define MODE NORMAL_MODE
-// #define MODE PARALLEL_MODE
 
 void pwm_task(void *arg)
 {
 
-	if (MODE == NORMAL_MODE)
+	motor_handle_t motor_a_0;
+	motor_handle_t motor_a_1;
+	ESP_ERROR_CHECK(enable_motor_driver(&motor_a_0, MOTOR_A_0));
+	ESP_ERROR_CHECK(enable_motor_driver(&motor_a_1, MOTOR_A_1));
+
+	// Make the Motors go forward & backward alternatively, at different PWM from 20 to 100 using a ramp function
+	while (1)
 	{
-		enable_motor_driver(a, NORMAL_MODE); // Enable motor driver A in Normal Mode
-
-		// Make the Motors go forward & backward alternatively, at different PWM from 60 to 100
-		while (1)
+		//first increase the PWM from 20 to 100 ramping up
+		for (int i = 20; i <=  70; i++)
 		{
-			for (int duty_cycle = 60; duty_cycle <= 100; duty_cycle++)
-			{
-				// setting motor speed of MOTOR A0 in forward direction with duty cycle
-				set_motor_speed(MOTOR_A_0, MOTOR_FORWARD, duty_cycle);
-
-				// setting motor speed of MOTOR A1 in forward direction with duty cycle
-				set_motor_speed(MOTOR_A_1, MOTOR_FORWARD, duty_cycle);
-
-				// adding delay of 100ms
-				vTaskDelay(100 / portTICK_PERIOD_MS);
-			}
-
-			// stopping the MOTOR A0
-			set_motor_speed(MOTOR_A_0, MOTOR_STOP, 0);
-
-			// stopping the MOTOR A1
-			set_motor_speed(MOTOR_A_1, MOTOR_STOP, 0);
-
-			// adding delay of 100ms
-			vTaskDelay(100 / portTICK_PERIOD_MS);
-
-			for (int duty_cycle = 60; duty_cycle <= 100; duty_cycle++)
-			{
-				// setting motor speed of MOTOR A0 in backward direction with duty cycle
-				set_motor_speed(MOTOR_A_0, MOTOR_BACKWARD, duty_cycle);
-
-				// setting motor speed of MOTOR A1 in backward direction with duty cycle
-				set_motor_speed(MOTOR_A_1, MOTOR_BACKWARD, duty_cycle);
-
-				// adding delay of 100ms
-				vTaskDelay(100 / portTICK_PERIOD_MS);
-			}
-
-			// stopping the MOTOR A0
-			set_motor_speed(MOTOR_A_0, MOTOR_STOP, 0);
-
-			// stopping the MOTOR A1
-			set_motor_speed(MOTOR_A_1, MOTOR_STOP, 0);
-
-			// adding delay of 100ms
+			set_motor_speed(motor_a_0, MOTOR_FORWARD, i);
+			set_motor_speed(motor_a_1, MOTOR_FORWARD, i);
 			vTaskDelay(100 / portTICK_PERIOD_MS);
 		}
-	}
-
-	else if (MODE == PARALLEL_MODE)
-	{
-		enable_motor_driver(a, PARALLEL_MODE); // Enable motor driver A in Parallel Mode
-
-		// Make the Motors go forward & backward alternatively, at different PWM from 60 to 100
-		while (1)
+		//then decrease the PWM from 100 to 20 ramping down
+		//add some delay between the two ramps
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+		for (int i = 70; i >= 20; i--)
 		{
-			for (int duty_cycle = 60; duty_cycle <= 100; duty_cycle++)
-			{
-				// setting motor speed of motor A in forward direction with duty cycle
-				set_motor_speed(MOTOR_A_0, MOTOR_FORWARD, duty_cycle);
-
-				// adding delay of 100ms
-				vTaskDelay(100 / portTICK_PERIOD_MS);
-			}
-
-			// stopping the motor A
-			set_motor_speed(MOTOR_A_0, MOTOR_STOP, 0);
-
-			// adding delay of 100ms
-			vTaskDelay(100 / portTICK_PERIOD_MS);
-
-			for (int duty_cycle = 60; duty_cycle <= 100; duty_cycle++)
-			{
-				// setting motor speed of motor A in backward direction with duty cycle
-				set_motor_speed(MOTOR_A_0, MOTOR_BACKWARD, duty_cycle);
-
-				// adding delay of 100ms
-				vTaskDelay(100 / 10);
-			}
-
-			// stopping the motor A
-			set_motor_speed(MOTOR_A_0, MOTOR_STOP, 0);
-
-			// adding delay of 100ms
+			set_motor_speed(motor_a_0, MOTOR_FORWARD, i);
+			set_motor_speed(motor_a_1, MOTOR_FORWARD, i);
 			vTaskDelay(100 / portTICK_PERIOD_MS);
 		}
+		//add some delay between the two ramps
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
 	}
 }
 
